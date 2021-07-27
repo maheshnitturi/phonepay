175f1f9d57b411fda0a4901543801bd927c1d448
unknown <maheshnitturi8@gmail.com>
2021-07-27 17:10:57 +0530

    added new files

---
+++ recharge/airtel-jio-idea/src/test/java/TestCommandPlugin.java
@@ -0,0 +1,19 @@
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import schemacrawler.tools.executable.CommandRegistry;
+
+public class TestCommandPlugin
+{
+
+  @Test
+  public void testCommandPlugin()
+    throws Exception
+  {
+    final CommandRegistry registry = new CommandRegistry();
+    assertTrue(registry.isCommandSupported("additional"));
+  }
+
+}
